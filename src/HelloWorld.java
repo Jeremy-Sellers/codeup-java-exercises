08b0f0b2d71ac946c5e1ca192366acc54a23197a
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-03-24 15:56:51 -0500

    completed syntax, types, and variables exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,58 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        int myFavoriteNumber = 17;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is a string";
+        System.out.println(myString);
+
+        long myNumber = 123;
+//           float myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);//Post increment runs after entire code block runs
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);//Pre-increment runs before value is printed out
+//        System.out.println(x);
+
+//        String class = "hello";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o; //String "three" cannot be parsed to a number
+//        System.out.println(three);
+
+//        int x = 4;
+//        x = x + 5;
+//            int x = 4;
+//            x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+////        y = y - x;
+//        x /= y;
+//        System.out.println(x);
+//        y -= x;//returns -3 due to x changing value above
+//        System.out.println(y);
+
+//        int maxIntVal = Integer.MAX_VALUE;
+//        int minIntVal = Integer.MIN_VALUE;
+//        System.out.println("Int max value is: " + maxIntVal);
+//        System.out.println("Int min value is: " + minIntVal);
+
+
     }
 }
