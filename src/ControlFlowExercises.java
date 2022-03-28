0a5a2c9ac7631afcaef1695d329ab59ee7a1476a
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-03-28 15:58:33 -0500

    completed first problem block

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class ControlFlowExercises {
+    public static void main(String[] args){
+        //Loop Basics a. While loops
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+        //Do while
+//        int start = 0;
+//        do {
+//            System.out.println(start += 2);
+//
+//        }while(start < 100);
+//
+//        do {
+//            System.out.println(start -= 5);
+//        }while(start > -10 );
+
+//        long startsAtTwo = 2;
+//        System.out.println(startsAtTwo);
+//        do {
+//            System.out.println(startsAtTwo *= startsAtTwo);
+//        }while(startsAtTwo < 257);
+
+//        for (int i = 5; i <= 15;i++){
+//            System.out.println(i);
+//        }
+//        for (int i = 0; i <= 100; i+=2){
+//            System.out.println(i);
+//        }
+
+//          for(int i = 100; i >= -10; i -=5){
+//              System.out.println(i);
+//          }
+
+//            for (long i = 2; i < 1_000_000; i*=i){
+//                System.out.println(i);
+//            }
+
+
+    }
+}
