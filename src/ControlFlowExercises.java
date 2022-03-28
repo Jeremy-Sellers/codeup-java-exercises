4708ba72530119998f3c447541ebadafc92a49e2
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-03-28 16:09:04 -0500

    finished fizzbuzz problem

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -39,6 +39,23 @@ public class ControlFlowExercises {
 //                System.out.println(i);
 //            }
 
+        //FizzBuzz
+//            for(int i =1; i <= 100; i++){
+//                if(i % 3 == 0 & i % 5 == 0){
+//                    System.out.println("FizzBuzz");
+//                }
+//                else if(i % 3 == 0){
+//                    System.out.println("Fizz");
+//                }else if(i % 5 == 0){
+//                    System.out.println("Buzz");
+//                }
+//                else{
+//                System.out.println(i);}
+//
+//                }
 
+
+
+
+            }
     }
-}
