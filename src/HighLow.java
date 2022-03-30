d75214803379db743c27c04d2d8c9b4480117fa3
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-03-30 15:39:30 -0500

    updated code for bonus specs. keeps track of attempts and cuts off after predetermined amount

--- src/HighLow.java
+++ src/HighLow.java
@@ -3,9 +3,10 @@ public class HighLow{
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         int randomNumber = (int) (Math.random()*100);
-        System.out.println(randomNumber);
-        System.out.println("Guess what number was picked between 0 and 100!");
+//        System.out.println(randomNumber);
+        System.out.println("Guess what number was picked between 0 and 100!\nYou will have 10 attempts!");
         int userGuess = userInput.nextInt();
+        int attempts = 1;
         while(userGuess != randomNumber) {
             if (userGuess < randomNumber) {
                 System.out.println("HIGHER");
@@ -16,7 +17,13 @@ public class HighLow{
                 System.out.println("Guess again!");
                 userGuess = userInput.nextInt();
             }
+            attempts++;
+            if (attempts == 10){
+                System.out.println("You have used all 10 attempts");
+                return;
+            }
         }
         System.out.println("GOOD GUESS!");
+        System.out.println("Attempts made: " + attempts);
     }
 }
