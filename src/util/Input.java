8786076caa5ef136a83e6653bcf655a7d5481532
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-04-07 17:00:26 -0500

    Completed Exceptions exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -11,7 +11,6 @@ public class Input {
     }
 
     String getString(){
-        System.out.println("Enter name: ");
         return scanner.nextLine();
     }
 
@@ -27,36 +26,68 @@ public class Input {
 
     int getInt(int min, int max){
         System.out.println("Enter a number between " + min + " and " + max);
-        int userInt = scanner.nextInt();
-        while(userInt > max | userInt < min){
+        String userInt = getString();
+        try{
+        while(Integer.valueOf(userInt) > max | Integer.valueOf(userInt) < min){
             System.out.println("Please enter a valid number");
-            userInt = scanner.nextInt();
+            userInt = getString();
         }
-        return userInt;
+            System.out.println("Your number: " + userInt + " is within the range.");
+        return Integer.valueOf(userInt);
+        }
+        catch (NumberFormatException NFE){
+            System.out.println("An error of " + NFE + " occured");
+            return getInt(min,max);
+        }
     }
 
     int getInt(){
         System.out.println("What's your favorite number?");
-        return scanner.nextInt();
+        String userInput = getString();
+        try{
+        System.out.println("Your number is " + Integer.valueOf(userInput));
+        return Integer.valueOf(userInput);
+    }
+        catch (NumberFormatException NFE){
+            System.out.println("An error of " + NFE + " occurred");
+            return getInt();
+        }
     }
 
     double getDouble(double min, double max){
-        System.out.println("Enter a large number larger than " + min + " but less than " + max);
-        double userDouble = scanner.nextDouble();
-        while(userDouble > max | userDouble < min){
+        System.out.println("Enter a number larger than " + min + " but less than " + max);
+        String userDouble = getString();
+        try{
+        while(Integer.valueOf(userDouble) > max | Integer.valueOf(userDouble) < min){
             System.out.println("Please enter a valid number");
-            userDouble = scanner.nextDouble();
+            userDouble = getString();
         }
-        return userDouble;
+        System.out.println("Your number: " + userDouble + " is within range.");
+        return Integer.valueOf(userDouble);
+        }catch (NumberFormatException NFE){
+            System.out.println("An error of type " + NFE + " occurred");
+            return getDouble(min,max);
+        }
     }
 
     public double getDouble(){
         System.out.println("Please enter a number");
-        return scanner.nextDouble();
+        String userNumber = getString();
+        try{
+            System.out.println("Your entered number " + Integer.valueOf(userNumber));
+            return Integer.valueOf(userNumber);
+        }
+        catch (NumberFormatException NFE){
+            System.out.println("An error of type " + NFE + " occurred");
+            return getDouble();
+        }
     }
 
-
-
-
+    public static void main(String[] args) {
+        Input newInput = new Input();
+//        newInput.getString();
+//        newInput.getInt(5,10);
+        newInput.getDouble(2,8);
+    }
 }
 
